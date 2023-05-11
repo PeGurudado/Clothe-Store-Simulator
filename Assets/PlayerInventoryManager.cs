3708852d90ad8727323084f56edf7161f354db36
Pedro Moraes <morkim30@gmail.com>
2023-05-10 21:29:48 -0300

    Added PlayerInventoryManager which opens player's inventory by Input, and shows by the function UpdateInventoryUI. Added body and clothes sprites.

---
+++ Assets/PlayerInventoryManager.cs
@@ -0,0 +1,49 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerInventoryManager : MonoBehaviour
+{
+    [SerializeField] PlayerItemsManager playerItemsManager;
+    [SerializeField] InventoryItemHolder inventoryItemPrefab;
+    [SerializeField] GameObject playerInventory;
+    [SerializeField] RectTransform inventoryListContent;
+
+    Dictionary<string, InventoryItemHolder> instantiatedInventoryItems = new Dictionary<string, InventoryItemHolder>();
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.I))
+        {
+            if (playerInventory.activeInHierarchy)
+            {
+                playerInventory.SetActive(false);
+                return;
+            }
+            //Opens Inventory
+            UpdateInventoryUI();
+            playerInventory.SetActive(true);
+        }
+
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            //Closes Inventorys
+            playerInventory.SetActive(false);
+        }
+        
+    }
+
+    void UpdateInventoryUI()
+    {
+        foreach (var ownedItem in playerItemsManager.ownedItems)
+        {
+            if (instantiatedInventoryItems.ContainsKey(ownedItem.Value.Name)) continue;
+
+            InventoryItemHolder newItem = Instantiate(inventoryItemPrefab, inventoryListContent.transform);
+            newItem.gameObject.SetActive(true);
+            newItem.Item = ownedItem.Value;
+            instantiatedInventoryItems.Add(ownedItem.Value.Name, newItem);
+        } 
+    }
+}
